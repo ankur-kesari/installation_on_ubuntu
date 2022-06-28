9a616096df78dcf5c324dc11535f1c40701c9ab6
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-06-28 11:40:44 +0530

    updated minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,6 +1,8 @@
 #!bin/bash
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install \
     ca-certificates \
     curl \
@@ -10,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 
