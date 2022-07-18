95a5fe1fb096baa375d5caae25c8548f29d26f15
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-07-18 12:03:28 +0530

    Update rancher.sh

--- rancher.sh
+++ rancher.sh
@@ -2,5 +2,6 @@
 curl https://releases.rancher.com/install-docker/20.10.sh | sh
 sudo docker run --privileged -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
 	
-
+#Passwaord-	
+#docker logs container-id 2>&1 | grep "Bootstrap Password:"
 
