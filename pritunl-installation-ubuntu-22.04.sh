d989e6dbb831b3db168469c2c650481aa0f5869b
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2023-09-12 15:14:34 +0530

    Create pritunl-installation-ubuntu-22.04.sh

---
+++ pritunl-installation-ubuntu-22.04.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt jammy main
+EOF
+
+# Import signing key from keyserver
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+# Alternative import from download if keyserver offline
+curl https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo apt-key add -
+
+sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list << EOF
+deb https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse
+EOF
+
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt --assume-yes upgrade
+sudo ufw disable
+sudo apt -y install pritunl mongodb-org unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo systemctl stop pritunl mongodb
+sleep 10
+sudo pritunl set-mongodb mongodb://localhost:27017/pritunl
+
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
+
+#to generate key
+sudo pritunl setup-key
+
+#to obtain default username and password
+sudo pritunl default-password
